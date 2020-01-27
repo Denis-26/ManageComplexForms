2a4b19f57d7fdf873896e2fd257d6d290e2147d4
deniskartavenko <kartavenkodenis0@gmail.com>
2020-01-27 23:56:21 +0300

    secon

---
+++ ManageComplexFormsTests/ManageComplexFormsTests.swift
@@ -0,0 +1,34 @@
+//
+//  ManageComplexFormsTests.swift
+//  ManageComplexFormsTests
+//
+//  Created by Denis Kartavenko on 27.01.2020.
+//  Copyright © 2020 Denis. All rights reserved.
+//
+
+import XCTest
+@testable import ManageComplexForms
+
+class ManageComplexFormsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
